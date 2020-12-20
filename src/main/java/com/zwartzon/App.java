ad553de844dabd8a8f1b6cf1ae6f37d2d1f09a56
Alexander <=>
2020-12-20 18:41:12 +0700

    HH-1 Initial commit

---
+++ src/main/java/com/zwartzon/App.java
@@ -0,0 +1,13 @@
+package com.zwartzon;
+
+import com.zwartzon.client.MainClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main(String[] args) {
+    MainClient.main(args);
+  }
+}
---
+++ src/test/java/com/zwartzon/AppTest.java
@@ -0,0 +1,18 @@
+package com.zwartzon;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
